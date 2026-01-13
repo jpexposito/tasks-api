2c2be645bd307799bf515946ed38e43db93f9847
Joatham Perez Exposito <jpe.gsc@gmail.com>
2026-01-13 19:18:15 +0000

    Se incluye la version hexagonal

--- src/main/java/com/docencia/tasks/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.docencia.tasks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docencia.tasks.entitys.TaskEntity;
-
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {}
